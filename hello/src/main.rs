5ec26534f353da3fa126cb21f4223e563b62523c
kz56cd <kz56cd@gmail.com>
2021-04-29 18:21:00 +0900

    Add first project

---
+++ hello/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // println!("Hello, world!");
+
+    for i in 1..=10 {
+      println!("Hello {} world!", to_ordinal_string(i));
+    }
+}
+
+fn to_ordinal_string(n: usize) -> String {
+  let s = match n % 10 {
+    1 => "st",
+    2 => "nd",
+    3 => "rd",
+    _ => "th",
+  };
+  format!("{}{}", n, s)
+} 

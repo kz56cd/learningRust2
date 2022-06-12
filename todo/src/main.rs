3e45912222aef4776cd20c9f3de916325c20480b
kz56cd <kz56cd@gmail.com>
2022-06-12 18:33:21 +0900

    Tweak web framework project

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,17 +1,49 @@
 use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
 use thiserror::Error;
 
+struct TodoEntry {
+    id: u32,
+    text: String
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<TodoEntry>,
+}
+
+// visit: http://localhost:8080
+
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+}
 
 // MyErrorは、actix_webのエラーに準拠させる
 impl ResponseError for MyError {}
 
 #[get("/")]
 async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello rust world!";
 
-    Ok(HttpResponse::Ok().body(response_body))
+    let mut entries = Vec::new();
+    entries.push(TodoEntry {
+        id: 1,
+        text: "First entry".to_string(),
+    });
+    entries.push(TodoEntry {
+        id: 2,
+        text: "Second entry".to_string(),
+    });
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+
+    // let response_body = "Hello rust world!";
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
@@ -23,3 +55,4 @@ async fn main() -> Result<(), actix_web::Error> {
         .await?;
     Ok(())
 }
+
